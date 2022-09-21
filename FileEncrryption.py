ce492bbe86893e6332d0080cc8e87c4f0f071e6c
Elizabeth Anne Pollitt <elizabethanne_polli1@baylor.edu>
2022-09-20 21:19:41 -0500

    more changes

--- FileEncrryption.py
+++ FileEncrryption.py
@@ -1,3 +1,78 @@
-infile = open('info_security.txt','r')
-outfile = open('encrypted.txt','w')
+codes = {'.':'6', ':':'0', "?":'8', ',':'1', 'E': '@', 'A': '$' ,'a':'5', "R": '!', 'B': '^', 'C': '~', "I": '(', "S": '&', "c": ')', "d":"q", "e":'g' ,'f':'+', 'g':';', 'h':'`', 'i':'|', 'j':',', 'k':'"', 'l':'-', 'm':'*', 'n':'<', 'o':'?', 'p':'>', 'q':'_', 'r':'e', 's':'/', 't':'G', 'u':"'", 'v':'X', 'w':'Q', 'x':'O', 'y':'W', 'z':"P"}
 
+infile = open('info_security.txt','r')
+oldtext = infile.read()
+text = oldtext.replace(" ", '')
+print(text)
+#text = infile.read()
+
+outfile = open('encrypted.txt','w')
+for character in text: 
+    text.rstrip()
+    if character in codes:
+        outfile.write(codes[character])
+    else:
+        outfile.write(character)
+outfile.close()
+outfile = open('info_security.txt','r')
+oldtext = outfile.read()
+text = oldtext.replace(" ",'')
+#text = outfile.read()
+outfile.close()
+codes_items = codes.items()
+for character in text:
+    if not character in codes.values() or character == '.':
+        print(character,end='')
+    else: 
+        for key,value in codes.items:
+            if character==value and character != '.':
+                print(key, end='')
+
+
+
+     #x = convert()
+     #outfile = open('encrypted.txt','w')
+     #outfile.write(x)
+     #outfile.close()
+
+
+
+#def convert ():
+    #infile = open('info_security.txt','r')
+    #x = ''
+    #text = infile.read()
+
+    #for character in text: 
+        #if character.isspace():
+            #x = x + character
+        #else:
+            #x = x + codes[character]
+
+    #return x 
+
+
+#def function (): 
+    #x = convert()
+    #print(x)
+
+#i#nfile = open('info_security.txt','r')
+#infile_read = infile.read()
+    
+
+#outfile = open('encrypted.txt','w')
+
+#codes = {'.':'6', ':':'0', "?":'8', ',':'1', 'E': '@', 'A': '$' ,'a':'5', "R": '!', 'B': '^', 'C': '~', "I": '(', "S": '&', "c": ')', "d":"q", "e":'g' ,'f':'+', 'g':';', 'h':'`', 'i':'|', 'j':',', 'k':'"', 'l':'-', 'm':'*', 'n':'<', 'o':'?', 'p':'>', 'q':'_', 'r':'e', 's':'/', 't':'G', 'u':"'", 'v':'X', 'w':'Q', 'x':'O', 'y':'W', 'z':"P"}
+
+#lines = infile.readlines()
+#lines  = [line.replace(" ", '') for line in lines]
+
+#for character in infile:
+    #character = codes
+    #print(character)
+
+#print(character)
+
+
+#outfile.writelines(lines)
+
+#outfile.close()
