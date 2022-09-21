9bf0f01880b0863467d015844e1e14686b96c24d
Elizabeth Anne Pollitt <elizabethanne_polli1@baylor.edu>
2022-09-20 22:11:01 -0500

    change

--- FileEncrryption.py
+++ FileEncrryption.py
@@ -1,33 +1,22 @@
-codes = {'.':'6', ':':'0', "?":'8', ',':'1', 'E': '@', 'A': '$' ,'a':'5', "R": '!', 'B': '^', 'C': '~', "I": '(', "S": '&', "c": ')', "d":"q", "e":'g' ,'f':'+', 'g':';', 'h':'`', 'i':'|', 'j':',', 'k':'"', 'l':'-', 'm':'*', 'n':'<', 'o':'?', 'p':'>', 'q':'_', 'r':'e', 's':'/', 't':'G', 'u':"'", 'v':'X', 'w':'Q', 'x':'O', 'y':'W', 'z':"P"}
 
-infile = open('info_security.txt','r')
-oldtext = infile.read()
-text = oldtext.replace(" ", '')
-print(text)
-#text = infile.read()
 
-outfile = open('encrypted.txt','w')
-for character in text: 
-    text.rstrip()
-    if character in codes:
-        outfile.write(codes[character])
-    else:
-        outfile.write(character)
-outfile.close()
-outfile = open('info_security.txt','r')
-oldtext = outfile.read()
-text = oldtext.replace(" ",'')
-#text = outfile.read()
-outfile.close()
-codes_items = codes.items()
-for character in text:
-    if not character in codes.values() or character == '.':
-        print(character,end='')
-    else: 
-        for key,value in codes.items:
-            if character==value and character != '.':
-                print(key, end='')
+def main():
+    code = {'.':'6', ':':'0', "?":'8', ',':'1', 'E': '@', 'A': '$' ,'a':'5', "R": '!', 'B': '^', 'C': '~', "I": '(', "S": '&', "c": ')', "d":"q", "e":'g' ,'f':'+', 'g':';', 'h':'`', 'i':'|', 'j':',', 'k':'"', 'l':'-', 'm':'*', 'n':'<', 'o':'?', 'p':'>', 'q':'_', 'r':'e', 's':'/', 't':'G', 'u':"'", 'v':'X', 'w':'Q', 'x':'O', 'y':'W', 'z':"P"}
+    encryption(code)
 
+def encryption(code):
+    infile = open('info_security.txt', 'r')
+    infile_read = infile.read()
+    encrypted = open('encrypted.txt', 'w')
+    for character in infile_read:
+        if character in code: 
+            encrypted.write(code[character])
+        else: 
+            encrypted.write(character)
+
+    encrypted.close()
+
+main()
 
 
      #x = convert()
